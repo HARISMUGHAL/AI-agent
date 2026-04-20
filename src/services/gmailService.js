aa0e3a0cdf26163e2d2b68dee99a101ca4015cb4
MUHAMMAD HARIS KAMRAN <harixmughal65@gmail.com>
2026-04-21 01:21:03 +0500

    Fix: remove duplicate declarations and validate gmailService.js

--- src/services/gmailService.js
+++ src/services/gmailService.js
@@ -29,7 +29,6 @@ const {
   recordEmailSentHealth,
   recordBounce,
   recordSpamComplaint,
-  recordSpamComplaint,
   recordSendError
 } = require('../database/db');
 const { rotateAccount } = require('./accountManager');
